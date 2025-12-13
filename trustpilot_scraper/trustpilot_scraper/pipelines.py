6be27017d7e2d7871e318ee809be9f6264884340
Baha Karakaya <bhkrky@gmail.com>
2025-12-13 17:28:32 +0300

    feat: Add DuplicateFilterPipeline and logging for item processing summary

--- trustpilot_scraper/trustpilot_scraper/pipelines.py
+++ trustpilot_scraper/trustpilot_scraper/pipelines.py
@@ -6,8 +6,47 @@
 
 # useful for handling different item types with a single interface
 from itemadapter import ItemAdapter
+from scrapy.exceptions import DropItem
+import logging
 
 
 class TrustpilotScraperPipeline:
     def process_item(self, item, spider):
         return item
+
+
+class DuplicateFilterPipeline:
+    def __init__(self):
+        self.urls_seen = set()
+
+        def process_item(self, item, spider):
+            unique_url = item['trustpilot_url'].strip().rstrip("/")
+
+            if unique_url in self.urls_seen:
+                raise DropItem(f"Duplicate data removed: {item['company_name']}")
+            else:
+                self.urls_seen.add(unique_url)
+                return item
+                
+
+class DescribeItemPipeline:
+    """A pipeline that describes unique items."""
+    def __init__(self) -> None:
+        self.categories = set()
+        self.subcategories = set()
+
+    def process_item(self, item, spider):
+        category = item.get('category')
+        subcategory = item.get('subcategory')
+        if category:
+            self.categories.add(category)
+        if subcategory:
+            self.subcategories.add(subcategory)
+        return item
+    
+    def close_spider(self, spider):
+        logging.info("------- SCRAPING SUMMARY -------")
+        logging.info(f"Total unique categories scraped: {len(self.categories)}")
+        logging.info(f"Categories: {self.categories}")
+        logging.info(f"Total unique subcategories scraped: {len(self.subcategories)}")
+        logging.info(f"Subcategories: {self.subcategories}")
